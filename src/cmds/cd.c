7f537a9f0c4e8cdbfad282320fdd1e62ac800032
Tsvetoslav Vasilev <tvasilev@c4c4c3.42berlin.de>
2023-02-14 11:53:17 +0100

    cd command

--- src/cmds/cd.c
+++ src/cmds/cd.c
@@ -1,24 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pwd.c                                              :+:      :+:    :+:   */
+/*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tvasilev <tvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/14 10:41:11 by tvasilev          #+#    #+#             */
-/*   Updated: 2023/02/14 11:35:06 by tvasilev         ###   ########.fr       */
+/*   Created: 2023/02/14 11:35:17 by tvasilev          #+#    #+#             */
+/*   Updated: 2023/02/14 11:52:45 by tvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
-#include "libft.h"
-#define PWD_SIZE 10000
 
-void	gs_pwd(void)
+//TODO cd without arguments goes to user home
+//TODO cd with '~' as an argument goes to user home
+//TODO if directory does not exist display 'cd: no such file or directory: {arg}'
+void	gs_cd(const char *path)
 {
-	char	*buf;
-
-	buf = xmalloc(PWD_SIZE * sizeof(char));
-	getcwd(buf, PWD_SIZE);
-	ft_printf("%s\n", buf);
+	chdir(path);
 }
