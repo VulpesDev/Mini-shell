498d3f650dd44736cf2688f40f6a406c43d5a282
zaperfish <zaperfish@gmail.com>
2023-02-14 11:31:19 +0100

    added xmalloc

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,11 +6,24 @@
 /*   By: lmiehler <lmiehler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 18:18:24 by lmiehler          #+#    #+#             */
-/*   Updated: 2023/02/13 18:18:35 by lmiehler         ###   ########.fr       */
+/*   Updated: 2023/02/14 11:29:34 by lmiehler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*xmalloc(size_t size)
+{
+	void *p;
+
+	p = malloc(size);
+	if (!p)
+	{
+		ft_fprintf(2, "Malloc failed\n");
+		exit(1);
+	}
+	return (p);
+}
 
 void	print_strs(char *str[])
 {
