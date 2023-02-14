24036c53dfcf5a1eaf4a353c074410b1720e2516
zaperfish <zaperfish@gmail.com>
2023-02-14 11:21:03 +0100

    branch testing

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: lmiehler <lmiehler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 17:38:06 by lmiehler          #+#    #+#             */
-/*   Updated: 2023/02/13 18:25:04 by lmiehler         ###   ########.fr       */
+/*   Updated: 2023/02/14 11:20:14 by lmiehler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void parser(t_meta *meta, const char *str)
 {
 	char **strs;
 
+	// Bullshit
 	strs = ft_split(str, ' '); //! if split == NULL - segf
 	meta->cmd = strs[0];
 	meta->cmd_args = &strs[1];
