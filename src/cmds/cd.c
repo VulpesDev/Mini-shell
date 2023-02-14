8543a919b5f0f96b9c30ac2f5e24d570249d95bd
zaperfish <zaperfish@gmail.com>
2023-02-14 12:52:13 +0100

    dunno

--- src/cmds/cd.c
+++ src/cmds/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tvasilev <tvasilev@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lmiehler <lmiehler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 11:35:17 by tvasilev          #+#    #+#             */
-/*   Updated: 2023/02/14 11:52:45 by tvasilev         ###   ########.fr       */
+/*   Updated: 2023/02/14 12:06:25 by lmiehler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 //TODO cd without arguments goes to user home
 //TODO cd with '~' as an argument goes to user home
 //TODO if directory does not exist display 'cd: no such file or directory: {arg}'
-void	gs_cd(const char *path)
+int	gs_cd(const char *path)
 {
 	chdir(path);
+	return (0);
 }
