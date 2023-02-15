1fc9eaa095d4dd44ed301a7426b5b00f148295fe
Tsvetoslav Vasilev <tvasilev@c4c4c3.42berlin.de>
2023-02-15 16:14:41 +0100

    Half-ass working export

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -6,7 +6,7 @@
 /*   By: tvasilev <tvasilev@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 18:17:56 by lmiehler          #+#    #+#             */
-/*   Updated: 2023/02/14 12:21:55 by tvasilev         ###   ########.fr       */
+/*   Updated: 2023/02/15 16:06:47 by tvasilev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,9 @@
 void	print_strs(char *str[]);
 
 void	*xmalloc(size_t size);
+
+int	size_dp(char **str);
+
+char	**cpy_dp(char **str);
 
 #endif
